51ba8f1234a316fcd373939863d8de7d6c7879dc
rupesh989 <rupeshbavaskar24>
2024-01-12 11:56:46 +0530

    added html and css files

---
+++ introduction.js
@@ -0,0 +1,24 @@
+// To write a first program we type this below function
+
+// create a new folder in your vs code(code editor)
+// create a new index.html file 
+// ! use emmets for boiler code 
+// use script tag for linking javascript file with html file
+// now open index file in chrome (browser)
+
+//console.log
+
+// "" double quotes are used to write any sentences(string )
+// this will print in your console statsments by click (CTRL + SHIFT + J) in windows chrome
+
+console.log("welcome to JavaScript Practice !!");
+
+// console.log("hello javascript");
+
+// alert window 
+//its a popup window that created when we use alert
+
+ alert("started javascript today");
+
+//it will show this pages says  
+//when clicked on ok then pop up window closes untill reloaded the browser
