83c2b3cf6480a109c8ddef9606f7111334b12dbd
RUPESH BAVASKAR <155056700+rupesh989@users.noreply.github.com>
2024-01-14 21:42:34 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,1 +1,22 @@
+// console.log("Promise Practice !!");
 
+// // let promise = new Promise ((resolve, reject) =>{
+// //     console.log("i am learning promises");  
+// //     // resolve(123);
+// //     // resolve("qwert");
+// //    // reject("some error accoured");
+// // });
+
+
+// // function getData (dataId, getNextData){
+
+// //     return new Promise((resolve, reject)=>{
+// //         setTimeout(() =>{
+// //             console.log("data",dataId);
+// //             resolve("success");
+// //             if (getNextData ){  
+// //                 getNextData();
+// //             }
+// //         }, 5000);
+// //     });
+// // }
