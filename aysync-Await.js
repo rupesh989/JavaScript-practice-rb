38f424d431dc9ae58c4d5df9fe022d0c855b747d
RUPESH BAVASKAR <155056700+rupesh989@users.noreply.github.com>
2024-01-15 09:34:46 +0530

    Update aysync-Await.js

--- aysync-Await.js
+++ aysync-Await.js
@@ -1,1 +1,22 @@
 
+console.log("async Awaits Practivce");
+
+async function hello(){
+    console.log("Hello !!");
+} //its returns promises fullfiled
+
+function api() {
+return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            console.log("weather data");
+            resolve(200);
+
+        }, 3000);
+    });
+}
+
+async function getWeatherData(){
+    await api(); //1st call
+    await api(); //2nd will work when 1st is completed
+}
+
