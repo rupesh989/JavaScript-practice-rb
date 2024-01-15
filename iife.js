37e5ebf834bfe4114dec00778648c13286a228fd
RUPESH BAVASKAR <155056700+rupesh989@users.noreply.github.com>
2024-01-15 09:31:51 +0530

    Update iife.js

--- iife.js
+++ iife.js
@@ -1,1 +1,27 @@
 
+//Invoked Function Expressions (IIFEs)
+
+function getData (dataId){
+    return new Promise ((resolve, reject) => {
+        setTimeout(() =>{
+            console.log("data",dataId);
+            resolve("success"); 
+        }, 3000);
+    });
+    }
+
+    
+(async function (){
+    console.log("wait for 3 sec your data is coming");
+    console.log("fetching data 77....");
+    await getData(77);
+    
+    console.log("fetching data 88....");
+
+    await getData(88);
+    console.log("fetching data 99....");
+
+    await getData(99);
+    console.log("End of data");
+
+})();
