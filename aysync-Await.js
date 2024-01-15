cae176330d3231b7f0615ed38bd0649c0230e262
RUPESH BAVASKAR <155056700+rupesh989@users.noreply.github.com>
2024-01-15 16:25:47 +0530

    Update aysync-Await.js

--- aysync-Await.js
+++ aysync-Await.js
@@ -20,3 +20,31 @@ async function getWeatherData(){
     await api(); //2nd will work when 1st is completed
 }
 
+
+function getData (dataId){
+    return new Promise ((resolve, reject) => {
+        setTimeout(() =>{
+            console.log("data",dataId);
+            resolve("success"); 
+        }, 3000);
+    });
+    }
+
+console.log("wait for 3 sec to get the your data");
+// getData(4).then ((res)=>{
+//     // console.log(res);
+//     console.log("fetching data 5....");
+//     getData(5).then((res)=>{
+//         // console.log(res);
+//         console.log("fetching data 6....");
+//         getData(6).then((res)=>{    
+//         // console.log(res);
+//         console.log("fetching data 7....");
+//             getData(7).then((res)=>{
+//             // console.log(res);
+//             console.log("ended the callbacks and no more data left here");
+//             });
+//         });
+//     });
+// });
+// 
