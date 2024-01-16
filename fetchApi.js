1e3997f2f26fcea616f0ab4de84984c229eca4ce
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-01-16 22:24:53 +0530

    Update fetchApi.js

--- fetchApi.js
+++ fetchApi.js
@@ -1,1 +1,17 @@
 
+console.log("Fetch API");
+
+//syntax 
+//let promise = fetch(URL,[Option])
+
+const URL = "https://cat-fact.herokuapp.com/facts";
+
+// const URL2= "http://dog-api.kinduff.com/api/facts";
+// let promise = fetch( URL);
+// console.log(promise);
+
+const factPara =document.querySelector("#facts");
+//check # for id atribute
+
+const btn1 =document.querySelector("#btn");
+// const btn2 = document.querySelector("#btn2");
