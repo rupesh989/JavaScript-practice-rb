2fe46d5dd5d67d59c1bc75b1a1e73cbf92ff56b0
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-28 22:12:01 +0530

    Update P2.js

--- P2.js
+++ P2.js
@@ -1,1 +1,18 @@
 
+//2. Question: Write a function that finds the longest word in a sentenc
+
+function findLongestWord(sentence) {
+  const words = sentence.split(' ');
+  let longestWord = '';
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
+}
+
+console.log(findLongestWord('The quick brown fox jumps over the lazy dog'));
+// Output: 'quick'
