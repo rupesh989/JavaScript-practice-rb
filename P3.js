857dc4e3872c624d4367813a92e29ef27128a352
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-28 22:13:54 +0530

    Update P3.js

--- P3.js
+++ P3.js
@@ -1,1 +1,8 @@
+// 3. Question: Write a function that removes duplicates from an array.
 
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+// Output: [1, 2, 3, 4, 5]
