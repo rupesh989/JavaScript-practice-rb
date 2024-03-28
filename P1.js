57ce8ed6589f01558c3a31cec93dc34fff0f263f
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-28 22:10:42 +0530

    Update P1.js

--- P1.js
+++ P1.js
@@ -1,1 +1,7 @@
+// Question: Write a function that reverses a given string.
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
 
+console.log(reverseString('Hello'));
+// Output: 'olleH'
