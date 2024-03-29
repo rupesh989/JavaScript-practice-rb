d5dc842d27d99674a91e8ff885c9bfdc48918bad
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-29 22:38:51 +0530

    Update P4.js

--- P4.js
+++ P4.js
@@ -1,1 +1,17 @@
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // Expected output: "resolved"
+}
+
+asyncCall();
 
