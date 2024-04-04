44d3e42f2685959c8f1cbed2447ffb03a803176b
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-04 17:49:15 +0530

    Update P6.js

--- P6.js
+++ P6.js
@@ -1,1 +1,17 @@
+// 1. Find the frequency of elements in array
+// Method 1: Using Reduce method of array
 
+
+let arr = ["hello", "world", "java", "hello", "java"]
+function countWord(p) {
+let result = p.reduce((allNames, name) {
+if (name in allNames) {
+allNames [name]++
+} else {
+allNames [name] = 1
+}
+return allNames;
+}, {});
+return result;
+}
+console.log(countWord(arr));
