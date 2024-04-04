f7407afce42456724d046a47148246e91cac7690
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-04 17:49:49 +0530

    Update P6.js

--- P6.js
+++ P6.js
@@ -15,3 +15,18 @@ return allNames;
 return result;
 }
 console.log(countWord(arr));
+
+// Method 2: Using an Object
+let arr = ["hello", "world", "java", "hello", "java"]
+function countWord(p) {
+var count = {};
+p.forEach((item) => {
+if (count[item]) {
+count[item]++
+} else {
+count[item] = 1
+}
+})
+return count;
+}
+console.log(countWord(arr));
