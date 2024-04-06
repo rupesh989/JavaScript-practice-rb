476b2d3ce1a5a81c7b83de2df04cc63bb89b5d29
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-06 22:15:22 +0530

    Update P11.js

--- P11.js
+++ P11.js
@@ -1,1 +1,6 @@
 
+// 7. Find the nth largest element in a sorted array
+let arr = [12,34,21,14,67,35,64,25]; arr.sort((a, b) => a b);
+const nthLargest = arr[arr.length n] // if n= 2
+console.log(nthLargest);
+// Output 64
