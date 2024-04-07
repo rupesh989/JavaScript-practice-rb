9ec4c195e63f94da817121a7c0c218c797e52962
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-07 17:46:47 +0530

    Update P13.js

--- P13.js
+++ P13.js
@@ -1,1 +1,13 @@
 // 8. Remove duplicates from an array and return unique values in O(n) complexity.
+let arr = [1, 2, 3, 4, 3, 6, 5, 4];
+function unique(arr) {
+let items = {}
+arr.forEach((item) => {
+if (!items[item]) {
+items [item] = item;
+}
+});
+return Object.values(items);
+}
+console.log(unique(arr));
+// Output [1,2,3,4,6,5]
