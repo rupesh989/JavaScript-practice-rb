008143e68779cc5f655dc2087b40a88e407b3865
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-09 22:26:47 +0530

    Update P16.js

--- P16.js
+++ P16.js
@@ -1,1 +1,15 @@
+//1. Write a function that reverses the order of words in a sentence without using the built-in reverse() method. 
 
+//This question not only assesses the creativity of the candidates but also helps hiring managers understand how well a candidate can come up with a clean and understandable solution. 
+
+//Sample answer: 
+
+function reverseSentence(sentence) { 
+
+  const words = sentence.split(‘ ‘); 
+
+  const reversedWords = words.reverse(); 
+
+  return reversedWords.join(‘ ‘); 
+
+} 
