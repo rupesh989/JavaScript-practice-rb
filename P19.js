045339f55632481afc6da77bb919dce737765a26
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-10 16:47:01 +0530

    Update P19.js

--- P19.js
+++ P19.js
@@ -1,1 +1,19 @@
+4. Implement a function that returns the average value of numbers in an array. 
 
+By asking this question, hiring managers can assess the candidate’s knowledge of arithmetic operations, array manipulation, and looping.  
+
+Sample answer: 
+
+function calculateAverage(numbers) { 
+
+  let sum = 0; 
+
+  for (let number of numbers) { 
+
+    sum += number; 
+
+  } 
+
+  return sum / numbers.length; 
+
+} 
