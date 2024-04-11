a176a8dc55d455c3249b57a54fbcd54287c9b496
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-11 19:16:59 +0530

    Update P22.js

--- P22.js
+++ P22.js
@@ -1,1 +1,13 @@
+7. Write a function that removes all falsy values (false, null, 0, “”, undefined, and NaN) from an array. 
+
+Candidates must showcase communication skills and explain their solutions logically. Interviewers analyze the interviewee’s ability to write a function that filters false values from an array.
+
+Sample answer: 
+
+function removeFalsyValues(arr) { 
+
+  return arr.filter(Boolean); 
+
+} 
+
 
