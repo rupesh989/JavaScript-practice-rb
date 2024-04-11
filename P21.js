903b11452f9cb0bb386e1762679cd546fbfebbcf
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-11 19:16:22 +0530

    Update P21.js

--- P21.js
+++ P21.js
@@ -1,1 +1,13 @@
+6. Implement a function that finds the index of a specific element in an array. If the element is not found, the function should return -1. 
 
+Interviewers aim to gauge the candidate’s proficiency in use of array methods, handling edge cases, and in JavaScript syntax. Candidates must implement the function proper error handling. 
+
+Sample answer: 
+
+function findElementIndex(arr, element) { 
+
+  const index = arr.indexOf(element); 
+
+  return index !== -1 ? index : -1; 
+
+} 
