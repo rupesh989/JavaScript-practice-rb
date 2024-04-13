af711a44341a5bd0079b5c98322853597a784947
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-13 18:32:21 +0530

    Update P27.js

--- P27.js
+++ P27.js
@@ -1,1 +1,19 @@
+2. Implement a function to reverse a string without using the built-in reverse() method. 
 
+Hiring managers aim to analyze the candidate’s knowledge of string manipulation in JavaScript while also measuring their ability to think of alternative solutions. 
+
+Sample answer: I would use a for lopp to iterate through the characters from the end to the beginning. By appending the character to a new string, it results in the reversed output. 
+
+function reverseString(str) { 
+
+  let reversed = ”; 
+
+  for (let i = str.length – 1; i >= 0; i–) { 
+
+    reversed += str[i]; 
+
+  } 
+
+  return reversed; 
+
+} 
