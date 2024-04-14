7e49fcf63f7a20c61246f91e4f5381507d1713b3
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-14 23:19:44 +0530

    Update P29.js

--- P29.js
+++ P29.js
@@ -1,1 +1,11 @@
+4. Write a function that takes an array of integers as input and returns a new array with only the unique elements. 
 
+Hiring managers can evaluate the candidate’s knowledge of JavaScript functions, array handling capabilities, and communicating technical concepts. 
+
+Sample answer: 
+
+function getUniqueElements(arr) { 
+
+  return Array.from(new Set(arr)); 
+
+} 
