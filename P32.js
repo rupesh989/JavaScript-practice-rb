139a28bcfd498705058161801360ebe44930e9c2
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-16 15:51:44 +0530

    Update P32.js

--- P32.js
+++ P32.js
@@ -1,1 +1,11 @@
+7. Implement a function to find the sum of all the numbers in an array. 
 
+Such a question helps understand if the interviewee can manipulate arrays and handle numeric values. This also helps managers assess problem-solving capabilities and ability to pay attention to code efficiency. 
+
+Sample answer: I would use the reduce method to implement the following function: 
+
+function findSum(arr) { 
+
+  return arr.reduce((sum, num) => sum + num, 0); 
+
+} 
