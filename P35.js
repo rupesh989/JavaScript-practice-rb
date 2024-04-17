e48137921c947f73fdb527fee80bc5135b44e45a
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-17 18:34:42 +0530

    Update P35.js

--- P35.js
+++ P35.js
@@ -1,1 +1,11 @@
+10. Write a function that sorts an array of numbers in ascending order. 
 
+Interviewees must show their knowledge of bubble sort, merge sort, sorting algorithms, and other approaches. The HR manager aims to measure the capability to execute strong algorithms and handle edge cases. 
+
+Sample answer: I can solve this by using JavaScript’s built-in sort method. 
+
+function ascendingSort(numbers) { 
+
+  return numbers.sort((a, b) => a – b); 
+
+}
