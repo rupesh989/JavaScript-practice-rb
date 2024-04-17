2d2a900cb9a9918bdbddebca3fce6c7dcac9f031
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-17 18:34:11 +0530

    Update P34.js

--- P34.js
+++ P34.js
@@ -1,1 +1,11 @@
+9. Implement a function to remove duplicates from an array. 
 
+When interviewers present the candidate with this question, they can gauge the level of understanding a candidate has regarding array methods and different approaches to solve the problem. 
+
+Sample answer: The following function duplicates from an array by converting it into a Set. This automatically removes duplicates. Next, the function converts the Set back into an array. 
+
+function removeDuplicates(arr) { 
+
+  return Array.from(new Set(arr)); 
+
+} 
