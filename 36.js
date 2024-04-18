21eef1c065a12af7b5206e8da27c92bf66e4064a
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-04-18 23:47:15 +0530

    Update 36.js

--- 36.js
+++ 36.js
@@ -1,1 +1,19 @@
+1. Implement a debounce function in JavaScript that limits the frequency of a function’s execution when it’s called repeatedly within a specified time frame. 
 
+Interviewers expect the candidate to showcase their ability to clearly explain the purpose of the debounce function and its usage in scenarios where function calls need to be controlled. They are looking for the person’s ability to articulate technical concepts clearly. 
+
+Sample answer: By delaying the execution of the debounce function until the specified time frame has passed, the frequency can be limited. 
+
+function debounce(func, delay) { 
+
+  let timer; 
+
+  return function() { 
+
+    clearTimeout(timer); 
+
+    timer = setTimeout(func, delay); 
+
+  }; 
+
+} 
